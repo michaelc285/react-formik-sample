866736b77e421ced4c4fcbc15848f723ccf407b1
Michael <michaelc285@gmail.com>
2021-07-22 22:46:09 +0800

    Update NavigationBarComponent
    Change a tag to Link

--- src/components/NavigationBarComponent.js
+++ src/components/NavigationBarComponent.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { ROOT, BASIC_FORM } from "../path";
+import { Link, useLocation } from "react-router-dom";
 
 export default function NavigationBarComponent() {
+    const location = useLocation();
+    const { pathname } = location;
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand px-2" href={ROOT}>Formik</a>
+            <Link className={`navbar-brand px-2`} to={ROOT}>Formik</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                        <a className="nav-link" href={ROOT}>Home</a>
+                        <Link className={`nav-link ${ROOT === pathname && "active"}`} to={ROOT}>Home</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href={BASIC_FORM}>BasicForm</a>
+                        <Link className={`nav-link ${BASIC_FORM === pathname && "active"}`} to={BASIC_FORM}>BasicForm</Link>
                     </li>
                 </ul>
             </div>
